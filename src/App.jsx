de6e5dfb6e3cfbe973517ce232fc35fca532f5c2
lucas id betan <lucas.idbetan@gmail.com>
2023-10-26 12:53:20 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center pt-20 ">
+        <h1>My portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
