83a41555cca4d4ffef5eccc4079a1266ffa45749
lucas id betan <lucas.idbetan@gmail.com>
2023-11-23 13:52:01 -0300

    Solving routes

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -2,59 +2,59 @@ export default [
     {   
         id:1,
         title:"HTML",
-        imgUrl:'src/assets/html5-logo-36.png'
+        imgUrl:'../src/assets/html5-logo-36.png'
         
     },
     {   
         id:2,
         title:"CSS",
-        imgUrl:"src/assets/css3-logo-36.png",
+        imgUrl:"../src/assets/css3-logo-36.png",
     
     },
     {   
         id:3,
         title:"JavaScript",
-        imgUrl:"src/assets/javascript-logo-36.png",
+        imgUrl:"../src/assets/javascript-logo-36.png",
     },
     {   
         id:4,
         title:"REACT",
-        imgUrl:"src/assets/react-logo-36.png",
+        imgUrl:"../src/assets/react-logo-36.png",
     
     },
     {   
         id:5,
         title:"TypeScript",
-        imgUrl:"src/assets/typescript-logo-36.png",
+        imgUrl:"../src/assets/typescript-logo-36.png",
     
     },
     {   
         id:6,
         title:"Bootstrap",
-        imgUrl:"src/assets/bootstrap-logo-36.png",
+        imgUrl:"../src/assets/bootstrap-logo-36.png",
     
     },
     {   
         id:7,
         title:"Tailwind",
-        imgUrl:"src/assets/tailwind-css-logo-36.png",
+        imgUrl:"../src/assets/tailwind-css-logo-36.png",
     
     },
     {   
         id:8,
         title:"SASS",
-        imgUrl:"src/assets/sass-logo-36.png",
+        imgUrl:"../src/assets/sass-logo-36.png",
     
     },
     {   
         id:9,
         title:"Firebase",
-        imgUrl:"src/assets/firebase-logo-36.png",
+        imgUrl:"../src/assets/firebase-logo-36.png",
     
     },
     {   
         id:10,
         title:"GIT",
-        imgUrl:"src/assets/git-logo-36.png",
+        imgUrl:"../src/assets/git-logo-36.png",
     },
 ]
