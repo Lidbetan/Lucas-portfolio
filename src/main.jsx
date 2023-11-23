a780353dbb3b7a9953a08fcd6eebdf0bbbe3ef51
lucas id betan <lucas.idbetan@gmail.com>
2023-11-23 13:26:00 -0300

    Solving path errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import './styles/tailwind.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ThemeContextProvider from './components/context/themeContextProvider'
+import ThemeContextProvider from "./components/context/ThemeContextProvider.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
