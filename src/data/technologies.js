003966628d9f2521ad62025669f3b6551d9cd7ac
lucas id betan <lucas.idbetan@gmail.com>
2024-01-10 11:54:00 -0300

    Added RTK logo to technologies

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -22,31 +22,37 @@ export default [
   },
   {
     id: 5,
+    title: "Redux Toolkit",
+    imgUrl: "/assets/rtk-logo.png",
+  },
+  
+  {
+    id: 6,
     title: "TypeScript",
     imgUrl: "/assets/typescript-logo-60.png",
   },
   {
-    id: 6,
+    id: 7,
     title: "Bootstrap",
     imgUrl: "/assets/bootstrap-logo-60.png",
   },
   {
-    id: 7,
+    id: 8,
     title: "Tailwind",
     imgUrl: "/assets/tailwind-css-logo-60.png",
   },
   {
-    id: 8,
+    id: 9,
     title: "SASS",
     imgUrl: "/assets/sass-logo-60.png",
   },
   {
-    id: 9,
+    id: 10,
     title: "Firebase",
     imgUrl: "/assets/firebase-logo-60.png",
   },
   {
-    id: 10,
+    id: 11,
     title: "GIT",
     imgUrl: "/assets/git-logo-60.png",
   },
