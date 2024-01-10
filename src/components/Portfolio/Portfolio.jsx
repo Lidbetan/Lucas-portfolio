ccb2a8c6bef5ba319c8d4d19bab79c49e0758ca4
lucas id betan <lucas.idbetan@gmail.com>
2024-01-10 11:46:59 -0300

    Normalized card project sizes

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,8 +8,8 @@ const Portfolio = () => {
         <h2 className="font-bold mb-20 text-2xl text-center uppercase text-slate-900 dark:text-slate-300">
           My projects
         </h2>
-        <div className="flex flex-col md:flex-row items-center justify-center ">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-9  text-black dark:text-white">
+        <div className="flex flex-col md:flex-row items-center justify-center mx-auto ">
+          <div className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-4 mx-auto mb-9  text-black dark:text-white">
             {portfolio.map((projects) => (
               <PortfolioItem
                 key={projects.id}
--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -1,7 +1,7 @@
 
 const PortfolioItem = ({title, imgUrl, stack, link }) => {
     return (
-        <div className='border-2 border-stone-900 dark:border-stone-300 rounded-md overflow-hidden'>
+        <div className='max-w-[425px] max-h-[340px] lg:h-[340px] border-2 border-stone-900 dark:border-stone-300 rounded-md overflow-hidden'>
             <div className='border-black border-b-{4px}'>
                 <a href={link}>
                     <img src={imgUrl}
@@ -10,7 +10,7 @@ const PortfolioItem = ({title, imgUrl, stack, link }) => {
                     </img>
                 </a>
             </div>
-            <div className='w-full p-4'>
+            <div className='p-4'>
                 <h3 className='text-center text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
                 <p className='flex flex-wrap gap-2 flew-row items-center justify-start text-xs md:text-sm '>
                     {stack.map((item, i) => (
