e5daeb81eda098eafdc580cf886049797c7dc86e
lucas id betan <lucas.idbetan@gmail.com>
2024-01-21 19:46:01 -0300

    Added target=_blank to all projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,28 +2,29 @@ import portfolio from "../../data/portfolio";
 import PortfolioItem from "./PortfolioItem";
 
 const Portfolio = () => {
-  return (
-    <>
-      <section className="my-14" id="projects">
-        <h2 className="font-bold mb-20 text-2xl text-center uppercase text-slate-900 dark:text-slate-300">
-          My projects
-        </h2>
-        <div className="flex flex-col md:flex-row items-center justify-center mx-auto ">
-          <div className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-4 mx-auto mb-9  text-black dark:text-white">
-            {portfolio.map((projects) => (
-              <PortfolioItem
-                key={projects.id}
-                imgUrl={projects.imgUrl}
-                title={projects.title}
-                stack={projects.stack}
-                link={projects.link}
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="my-14" id="projects">
+                <h2 className="font-bold mb-20 text-2xl text-center uppercase text-slate-900 dark:text-slate-300">
+                    My projects
+                </h2>
+                <div className="flex flex-col md:flex-row items-center justify-center mx-auto ">
+                    <div className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-4 mx-auto mb-9  text-black dark:text-white">
+                        {portfolio.map((projects) => (
+                            <a key={projects.id} href={projects.link} rel="noreferrer" target="_blank">
+                                <PortfolioItem
+                                    key={projects.id}
+                                    imgUrl={projects.imgUrl}
+                                    title={projects.title}
+                                    stack={projects.stack}
+                                />
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Portfolio;
