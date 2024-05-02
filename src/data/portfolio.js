696f28c8a2e8b6ba589d23effae86232c5acb5b5
lucas id betan <lucas.idbetan@gmail.com>
2024-05-02 11:28:01 -0300

    fixed Hiupy url

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,30 +1,69 @@
 export default [
-    {   
-        id:1,
-        title:"T-shirt e-commerce",
-        imgUrl:"/assets/rockandd-cover.png",
-        stack:["HTML ", "CSS ", "JavaScript ", "Bootstrap ", "React ", "Vite "],
-        link: "https://rockandd.netlify.app/"
+    {
+        id: 1,
+        title: "T-shirt e-commerce",
+        imgUrl: "/assets/rockandd-cover.png",
+        stack: [
+            "HTML ",
+            "CSS ",
+            "JavaScript ",
+            "Bootstrap ",
+            "React ",
+            "Vite ",
+        ],
+        link: "https://rockandd.netlify.app/",
     },
-    {   
-        id:2,
-        title:"Coffee Store e-commerce",
-        imgUrl:"/assets/locos-x-el-cafe.png",
-        stack:["HTML ", "CSS ", "JavaScript ", "Bootstrap ", "SASS"],
-        link: "https://locos-por-el-cafe.vercel.app/"
+    {
+        id: 2,
+        title: "Coffee Store e-commerce",
+        imgUrl: "/assets/locos-x-el-cafe.png",
+        stack: ["HTML ", "CSS ", "JavaScript ", "Bootstrap ", "SASS"],
+        link: "https://locos-por-el-cafe.vercel.app/",
     },
-    {   
-        id:3,
-        title:"Redux Toolkit - Postlist",
-        imgUrl:"/assets/rtk-post.png",
-        stack:["HTML ", "CSS ", "JavaScript ", "Tailwind ", "React ", "Redux", "Vite "],
-        link: "https://rtk-post-list.vercel.app/"
+    {
+        id: 3,
+        title: "Redux Toolkit - Postlist",
+        imgUrl: "/assets/rtk-post.png",
+        stack: [
+            "HTML ",
+            "CSS ",
+            "JavaScript ",
+            "Tailwind ",
+            "React ",
+            "Redux",
+            "Vite ",
+        ],
+        link: "https://rtk-post-list.vercel.app/",
     },
-    {   
-        id:4,
-        title:"Hiupy - Food Inventory",
-        imgUrl:"/assets/hiupy.png",
-        stack:["JavaScript ", "Tailwind ", "React ", "ContextApi", "Vite ", "NodeJs", "Express", "Sequelize"],
-        link: "https://hiupy-frontend.vercel.app/"
-    }
-]
+    {
+        id: 4,
+        title: "Hiupy - Food Inventory",
+        imgUrl: "/assets/hiupy.png",
+        stack: [
+            "JavaScript ",
+            "Tailwind ",
+            "React ",
+            "ContextApi",
+            "Vite ",
+            "NodeJs",
+            "Express",
+            "Sequelize",
+        ],
+        link: "https://hiupy.vercel.app/",
+    },
+    {
+        id: 5,
+        title: "TalkIAmos",
+        imgUrl: "/assets/talkiamos.png",
+        stack: [
+            "NextJS ",
+            "Tailwind",
+            "NextUI ",
+            "Zustand",
+            "Nest",
+            "MongoDB",
+            "OpenAI API",
+        ],
+        link: "https://talkiamos.vercel.app/",
+    },
+];
